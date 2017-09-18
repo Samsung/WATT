63a22f751dc4bba91796a88c2a7be80498db55ff
Hunseop Jeong <hs85.jeong@samsung.com>
2017-09-18 15:55:04 +0900

    Add beautify extension
    It makes minified script to be beautified code.
    
    Signed-off-by: Hunseop Jeong <hs85.jeong@samsung.com>

---
+++ libs/brackets-server/embedded-ext/hirse.beautify/nls/ko/strings.js
@@ -0,0 +1,4 @@
+define({
+    UNSUPPORTED_TITLE: '지원되지 않은 언어',
+    UNSUPPORTED_MESSAGE: '지원되지 않는 언어입니다. 지원되는 언어는 JavaScript, JSON, HTML, XML, SVG, HTML in PHP, Embedded JavaScript, Handlebars, CSS, SCSS, LESS 입니다.'
+});
