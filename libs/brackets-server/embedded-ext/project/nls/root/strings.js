2c9a99f18999e4d04634d564c4409c3a57b12718
Kwanghyuk-Kim <hyuki.kim@samsung.com>
2017-11-24 17:18:03 +0900

    Correct title of dialog for wgt packaging. (#82)
    - Modify title of wgt packing from build to pack
    - rename related string and function name
    
    Signed-off-by: KwangHyuk Kim <hyuki.kim@samsung.com>

--- libs/brackets-server/embedded-ext/project/nls/root/strings.js
+++ libs/brackets-server/embedded-ext/project/nls/root/strings.js
@@ -11,15 +11,18 @@ define({
     // Project - Compile
     "MENU_TITLE"                        : "Compile File",
     "DIALOG_TITLE"                      : "Compile File",
-    "MENU_TITLE_BUILD_WGT"              : "Build WGT package",
+    "MENU_TITLE_PACK_WGT"               : "Pack to WGT package",
     "MENU_TITLE_FILE_SELECT"            : "Select built files",
     "MENU_TITLE_OPTIONS_SELECT"         : "Select build options",
     "MENU_TITLE_PROJECT"                : "Compile Project",
     "MENU_TITLE_PROJECT_CLEAN"          : "Clean Project",
     "MENU_TITLE_PUBLISH"                : "Publish Project",
     "COMPILING_DIALOG_TITLE"            : "Compiling...",
+    "PACKING_DIALOG_TITLE"              : "Packing...",
     "COMPILATION_SUCCESS"               : "Success!",
     "COMPILATION_FAILED"                : "Fail!",
+    "PACKING_SUCCESS"                   : "Success!",
+    "PACKING_FAILURE"                   : "Failure!",
     "COMPILE_FILE_lABEL"                : "Compile File:",
     "CHANGE"                            : "Change…",
     "CHOOSE_COMPILE_TYPE_LABEL"         : "Compile Type:",
@@ -27,6 +30,5 @@ define({
     "CPP_COMPILE_OPTION_LABEL"          : "C++",
     "CMAKE_COMPILE_OPTION_LABEL"        : "CMake",
     "COMPILE_OPTION_LABEL"              : "Compile Options:",
-
     "PROJECT_MENU"                      : "Project"
 });
